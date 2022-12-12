f451bb861103eb50cb90b030bda4b1e462e580e2
Jimmy Woestman <jwoestman@cedar.com>
2022-12-12 13:28:37 -0600

    Streamlining

--- 2022/12/day_12.py
+++ 2022/12/day_12.py
@@ -1,36 +1,6 @@
 
-v = {
-    "-": 99,
-    "S": 0,
-    "a": 1,
-    "b": 2,
-    "c": 3,
-    "d": 4,
-    "e": 5,
-    "f": 6,
-    "g": 7,
-    "h": 8,
-    "i": 9,
-    "j": 10,
-    "k": 11,
-    "l": 12,
-    "m": 13,
-    "n": 14,
-    "o": 15,
-    "p": 16,
-    "q": 17,
-    "r": 18,
-    "s": 19,
-    "t": 20,
-    "u": 21,
-    "v": 22,
-    "w": 23,
-    "x": 24,
-    "y": 25,
-    "z": 26,
-    "E": 27
-}
-
+v = {char: i for i, char in enumerate("SabcdefghijklmnopqrstuvwxyzE")}
+v["-"] = 99
 
 def run():
     file = open("input.txt", "r")
@@ -101,7 +71,6 @@ def find_start_positions(grid):
 
     for k, row in enumerate(grid):
         for j, cell in enumerate(row):
-            # if cell in ["S"]:
             if cell in ["S", "a"]:
                 starts.append((k, j))
 
